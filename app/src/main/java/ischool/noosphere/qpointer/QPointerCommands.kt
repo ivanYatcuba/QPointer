9490d749aff72facd2c671c2cc3ff4a6fd7efa5c
iyatsouba <ivan.yatsouba@gmail.com>
2017-06-02 20:12:12 +0300

    updated commands

--- app/src/main/java/ischool/noosphere/qpointer/QPointerCommands.kt
+++ app/src/main/java/ischool/noosphere/qpointer/QPointerCommands.kt
@@ -2,7 +2,7 @@ package ischool.noosphere.qpointer
 
 enum class QPointerCommands(val command: String) {
 
-    SET_SECONDS("US"), SET_MINUTES("UM"), SET_HOURS("UH"),
+    SET_SECONDS("SS"), SET_MINUTES("SM"), SET_HOURS("SH"),
     SET_COLOR("UC"), SET_LASER_STATE("UL"), SET_ALARM_HOURS("AH"),
     SET_ALARM_MINUTES("AM"), SET_ALARM_OFF("AOFF")
 
